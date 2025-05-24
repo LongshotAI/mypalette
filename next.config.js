7c686d57aab798be03befe514e2c61a38897e8cf
LongshotAI <lshot.crypto@gmail.com>
2025-05-24 15:31:55 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
+
